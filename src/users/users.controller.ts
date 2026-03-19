bea0c9bcc44dafd0b8539fc1ca001fa84d7e0752
kunaljr <kunalshelke123@gmail.com>
2026-03-20 01:37:59 +0530

    Customer data Serialization.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,14 +3,19 @@ import {
   Post,
   Body,
   Get,
+  Patch,
   Param,
   Query,
   Delete,
+  NotFoundException,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
-
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private userService: UsersService) {}
   @Post('/signup')
@@ -19,8 +24,13 @@ export class UsersController {
   }
 
   @Get('/:id')
-  findUser(@Param('id') id: string) {
-    return this.userService.findOne(id);
+  async findUser(@Param('id') id: string) {
+    const user = await this.userService.findOne(id);
+    console.log(user);
+    if (!user) {
+      throw new NotFoundException('User Not Found!');
+    }
+    return user;
   }
 
   @Get()
@@ -31,5 +41,10 @@ export class UsersController {
   @Delete('/:id')
   removeUser(@Param('id') id: string) {
     return this.userService.remove(id);
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.userService.update(id, body);
   }
 }
